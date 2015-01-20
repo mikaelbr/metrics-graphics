007d11068f7ce96031ce8fd8986a89916d19e2ed
Mikael Brevik <mikaelbre@gmail.com>
2015-01-20 11:54:39 +0100

    Adds support for transition on confidence band

--- src/js/charts/line.js
+++ src/js/charts/line.js
@@ -28,9 +28,6 @@ charts.line = function(args) {
 
         //if it already exists, remove it
         var $existing_band = $(args.target).find('.mg-confidence-band');
-        if ($existing_band.length > 0) {
-            $existing_band.remove();
-        }
 
         if (args.show_confidence_band) {
             confidence_area = d3.svg.area()
@@ -62,6 +59,7 @@ charts.line = function(args) {
         //for building the optional legend
         var legend = '';
         var this_data;
+        var confidenceBand;
 
         for (var i = args.data.length - 1; i >= 0; i--) {
             this_data = args.data[i];
@@ -76,10 +74,20 @@ charts.line = function(args) {
 
             //add confidence band
             if (args.show_confidence_band) {
-                svg.append('path')
-                    .attr('class', 'mg-confidence-band')
-                    .attr('d', confidence_area(args.data[i]))
-                    .attr('clip-path', 'url(#mg-plot-window-'+ mg_strip_punctuation(args.target)+')');
+                if ($existing_band.length > 0) {
+                    confidenceBand = svg.select('.mg-confidence-band')
+                        .transition()
+                        .duration(function() {
+                          return (args.transition_on_update) ? 1000 : 0;
+                        });
+                } else {
+                    confidenceBand = svg.append('path')
+                        .attr('class', 'mg-confidence-band');
+                }
+
+              confidenceBand
+                  .attr('d', confidence_area(args.data[i]))
+                  .attr('clip-path', 'url(#mg-plot-window-'+ mg_strip_punctuation(args.target)+')');
             }
 
             //add the area
